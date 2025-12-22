37ff72b51068af1ae862408cc17588a8068c1e2b
deeep8250 <deepmondal825005@gmail.com>
2025-12-22 22:35:50 +0530

    added location table (migration)

--- Backend/internal/migrations/20251222023021_location_table.up.sql
+++ Backend/internal/migrations/20251222023021_location_table.up.sql
@@ -3,9 +3,16 @@ CREATE TABLE locations (
 id serial primary key,
 user_id integer not null,
 pin varchar(6) not null,
-state text  not null default "west-bengal",
-city text not null default "kolkata",
+state text  not null default 'west-bengal',
+city text not null default 'kolkata',
+village text not null ,
+owner_type text not null ,
+updated_at  timestamptz not null default now(),
+created_at timestamptz not null default now() ,
 
-
+constraint fk_locations_user
+foreign key (user_id)
+references users(id)
+on delete cascade
 
 );
